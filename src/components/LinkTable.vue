31345222cfd3bbf8af48063e53da1fb35a5a463e
jruaaa <joeyruan@zoho.com>
2021-05-27 14:01:51 -0700

    change links

--- src/components/LinkTable.vue
+++ src/components/LinkTable.vue
@@ -1,29 +1,24 @@
 <template>
 <table class="table-auto w-full text-sm">
     <tr>
-        <td><b>SELF</b></td>
-        <Link url="https://mail.google.com/mail/u/0/#inbox" name="gmail"/>
-        <Link url="https://www.facebook.com/" name="facebook"/>
+        <td><b>Read</b></td>
+        <Link url="https://misd.schoology.com" name="schoology"/>
+        <Link url="http://family.mercerisland.wa-k12.net/" name="skyward"/>
     </tr>
     <tr>
-        <td><b>READ</b></td>
-        <Link url="https://www.reddit.com/" name="reddit"/>
-        <Link url="https://medium.com/" name="medium"/>
+        <td><b>Write</b></td>
+        <Link url="https://docs.google.com" name="google docs"/>
+        <Link url="https://drive.google.com" name="google drive"/>
     </tr>
     <tr>
-        <td><b>SCHOOL</b></td>
-        <Link url="https://mail.google.com/mail/u/1/#inbox" name="mail"/>
-        <Link url="https://mica.instructure.com/" name="canvas"/>
-    </tr>
-    <tr>
-        <td><b>DEV</b></td>
-        <Link url="https://github.com/" name="github"/>
-        <Link url="https://stackoverflow.com/" name="stack overflow"/>
-    </tr>
-    <tr>
-        <td><b>DESIGN</b></td>
-        <Link url="https://www.awwwards.com/" name="awwwards."/>
-        <Link url="https://muz.li/" name="muzli"/>
+        <td><b>Class Links</b></td>
+        <Link url="https://mercerislandschools.zoom.us/j/95429474860" name="P1"/>
+        <Link url="https://mercerislandschools.zoom.us/j/85787280330" name="P3"/>
+        <Link url="https://mercerislandschools.zoom.us/j/93305194723?pwd=RjF0a2hDQzlUd3FqTzgwdU5IMDZwZz09" name="P7"/>
+        <Link url="https://mercerislandschools.zoom.us/j/98281543560" name="P2"/>
+        <Link url="https://mercerislandschools.zoom.us/j/91010712635" name="Islander Hour"/>
+        <Link url="https://mercerislandschools.zoom.us/j/96765745509" name="P4"/>
+        <Link url="https://mercerislandschools.zoom.us/j/96754655426?pwd=eFVKZzVhYk9TaHVQdHJMKzNMR3piZz09" name="P6"/>
     </tr>
 </table>
 </template>
