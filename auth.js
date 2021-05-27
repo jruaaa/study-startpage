06db0532b26feaf3d9d4f42334b310597739746a
Joey R <32626650+jruaaa@users.noreply.github.com>
2021-05-27 13:32:37 -0700

    add auth and cal

---
+++ auth.js
@@ -0,0 +1,5 @@
+export default {
+  api_key: 'AIzaSyD1HwENVlj3DC8u0ueAr17q0jiVqhlus48',
+  client_id: '165189172020-1u2vmptg0lap3p2vap0grh7aoid52auq.apps.googleusercontent.com',
+  scopes: 'calendar.readonly',
+};
---
+++ calendars.js
@@ -0,0 +1,1 @@
+export default { ids: ['08ibnad7msitcq6bms6086n7vo6589j1@import.calendar.google.com'] };
